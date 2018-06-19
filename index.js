7d405b1c0594d707dfa85eb28ded9c1c3b829796
Mariam Qasim <mariam.a.qasim@gmail.com>
2018-06-19 14:50:29 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express'); //common js vs es2015 module style
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send({ hi: 'there' });
+});
+
+//dynamic port binding
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);

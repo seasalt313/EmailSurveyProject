0aab1cf7b742e4cff11e420536d4bd55be0ec96b
Mariam Qasim <mariam.a.qasim@gmail.com>
2018-07-25 13:34:08 -0400

    Finished google auth setup, implemented redux, working on routing

--- services/passport.js
+++ services/passport.js
@@ -24,20 +24,17 @@ passport.use(
 			callbackURL: '/auth/google/callback',
 			proxy: true
 		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleId: profile.id }).then(existingUser => {
-				if (existingUser) {
-					//we already have a record with the given profile ID
-					done(null, existingUser);
-				} else {
-					//we dont have a user record with this ID - it returned as null- , make a new record
-					new User({
-						googleId: profile.id
-					})
-						.save()
-						.then(user => done(null, user));
-				}
-			});
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleId: profile.id });
+
+			if (existingUser) {
+				return done(null, existingUser);
+			}
+
+			const user = await new User({
+				googleId: profile.id
+			}).save();
+			done(null, user);
 		}
 	)
 );

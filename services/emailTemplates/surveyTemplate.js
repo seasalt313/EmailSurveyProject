b6ddb5a1658c8a160c4d6b55c9add9459e7129ea
Mariam Qasim <mariam.a.qasim@gmail.com>
2018-07-27 13:58:05 -0400

    Added sendgrid, emails are working now, and routing is fixed for surveys

---
+++ services/emailTemplates/surveyTemplate.js
@@ -0,0 +1,16 @@
+const keys = require('../../config/keys');
+module.exports = survey => {
+	return `
+  <html>
+  <body>
+    <div>
+      <h3>I'd like your input</h3>
+      <p>Please answer the following question: </p>
+      <p>${survey.body}</p>
+      <div><a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a></div>
+      <div><a href="${keys.redirectDomain}/api/surveys/thanks">No</a></div>
+    </div>
+  </body>
+  </html>
+  `;
+};

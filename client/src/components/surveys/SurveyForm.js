a25015ded285834f5dd1e348466d2b1bedd6fd49
Mariam Qasim <mariam.a.qasim@gmail.com>
2018-07-27 14:24:33 -0400

    Added custom field components

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,13 +2,22 @@
 
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
 
 class SurveyForm extends Component {
+	renderFields() {
+		return (
+			<div>
+				<Field type="text" name="surveyTitle" component={SurveyField} />
+			</div>
+		);
+	}
+
 	render() {
 		return (
 			<div>
 				<form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-					<Field type="text" name="surveyTitle" component="input" />
+					{this.renderFields()}
 					<button type="submit">Submit</button>
 				</form>
 			</div>

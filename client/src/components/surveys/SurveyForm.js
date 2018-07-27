617928cefd3c38bc39030bcceeca936455843cac
Mariam Qasim <mariam.a.qasim@gmail.com>
2018-07-27 15:01:04 -0400

    Added input errors for form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -45,6 +45,19 @@ class SurveyForm extends Component {
 	}
 }
 
+function validate(values) {
+	const errors = {};
+
+	_.each(FIELDS, ({ name }) => {
+		if (!values[name]) {
+			errors[name] = 'You must provide a value';
+		}
+	});
+
+	return errors;
+}
+
 export default reduxForm({
+	validate: validate,
 	form: 'surveyForm'
 })(SurveyForm);

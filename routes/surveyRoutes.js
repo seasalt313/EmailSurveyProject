a2d00ec05fbae879f2df9741e5d83420627c0d06
Mariam Qasim <mariam.a.qasim@gmail.com>
2018-07-30 15:52:34 -0400

    Trying to debug localtunnel and sendgrid, logs arent working

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -11,6 +11,16 @@ module.exports = app => {
 	app.get('/api/surveys/thanks', (req, res) => {
 		res.send('Thanks for voting!');
 	});
+
+	app.post('/api/surveys/webhooks', (req, res) => {
+		console.log('====================================');
+		console.log('hello');
+		console.log('====================================');
+		console.log(req.body);
+
+		res.send({});
+	});
+
 	app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
 		const { title, subject, body, recipients } = req.body;
 
